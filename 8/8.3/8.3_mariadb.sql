37715b60d0bdfb952fb5fe3bff1ed71230991058
Daria Postolatii <dariya2303@@gmail.com>
2022-07-14 22:09:29 +0300

    Добавлено задание 8.3

---
+++ 8/8.3/8.3_mariadb.sql
@@ -0,0 +1,17 @@
+create table users
+(
+    id                int unsigned                          not null,
+    lastname          varchar(64)                           not null,
+    firstname         varchar(32)                           not null,
+    birthdate         date                                  not null,
+    email             varchar(255)                          not null,
+    phone             varchar(13)                           not null,
+    gender            enum ('male', 'female')               not null,
+    timezone          varchar(37)                           not null,
+    registration_time timestamp current_timestamp()         not null,
+    registration_ip   varchar(15)                           not null,
+    UNIQUE (id),
+    UNIQUE (email),
+    PRIMARY KEY (id),
+    CHECK ( phone = 13)
+);
