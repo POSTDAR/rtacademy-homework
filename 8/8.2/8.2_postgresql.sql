407707ecc83f60bce0f231be6f9afff450ef7827
Daria Postolatii <dariya2303@@gmail.com>
2022-07-14 23:24:34 +0300

    Исправлено задание 8.2

--- 8/8.2/8.2_postgresql.sql
+++ 8/8.2/8.2_postgresql.sql
@@ -1,14 +1,14 @@
 create type gender_request as enum('male', 'female');
 CREATE TABLE users (
                        id serial,
-                       lastname char (64),
-                       firstname char (32),
+                       lastname varchar (64),
+                       firstname varchar (32),
                        birthdate bytea,
-                       email char(255),
-                       phone char(13),
+                       email varchar(255),
+                       phone varchar(13),
                        gender gender_request,
-                       timezone char(37),
+                       timezone varchar(37),
                        registration_time timestamp,
-                       registration_ip char(15)
+                       registration_ip cidr (15)
 
 )
