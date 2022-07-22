c74332b61ecf86489d72cd26cfdf40a035be061c
Daria Postolatii <dariya2303@@gmail.com>
2022-07-22 16:26:04 +0300

    Исправлено задание 8.6, таблица category

--- 8/8.6/8.6_category.sql
+++ 8/8.6/8.6_category.sql
@@ -1,5 +1,5 @@
 CREATE TABLE category(
-         id  int PRIMARY KEY    NOT NULL,
+         id  int PRIMARY KEY    NOT NULL AUTO_INCREMENT,
          title varchar(64)      NOT NULL,
          alias varchar(64)      NOT NULL UNIQUE
 )
