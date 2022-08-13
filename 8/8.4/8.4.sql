0fbde5be28421d35b4559dfd755ff9149815d3a1
Daria Postolatii <dariya2303@@gmail.com>
2022-08-13 17:49:10 +0300

    В задании 8,4 добавлено во все id -  AUTO INCREMENT исправленна строка 39 на тип данных int

--- 8/8.4/8.4.sql
+++ 8/8.4/8.4.sql
@@ -1,42 +1,42 @@
 CREATE TABLE manager(
-                        id            int NOT NULL PRIMARY KEY ,
+                        id            int NOT NULL PRIMARY KEY AUTO_INCREMENT,
                         first_name    varchar(64) NOT NULL ,
                         last_name     varchar(64) NOT NULL
 );
 
 CREATE TABLE client (
-                        id            int NOT NULL PRIMARY KEY ,
+                        id            int NOT NULL PRIMARY KEY AUTO_INCREMENT,
                         first_name    varchar(64) NOT NULL ,
                         last_name     varchar(64) NOT NULL,
                         phone           varchar(13)NOT NULL
 );
 
 CREATE TABLE address(
-                        id            int NOT NULL PRIMARY KEY ,
+                        id            int NOT NULL PRIMARY KEY AUTO_INCREMENT,
                         address varchar(225) NOT NULL
 );
 
 CREATE TABLE client_addresses(
-                                 id int NOT NULL PRIMARY KEY ,
+                                 id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
                                  client_id int,
                                  client_address varchar,
                                  FOREIGN KEY (client_id) REFERENCES client(id),
                                  FOREIGN KEY (client_address) REFERENCES address(id)
 );
 CREATE TABLE product(
-                        id  int NOT NULL PRIMARY KEY ,
+                        id  int NOT NULL PRIMARY KEY AUTO_INCREMENT,
                         product_name varchar NOT NULL UNIQUE ,
                         price int UNSIGNED
 );
 CREATE TABLE discount(
-                         id int NOT NULL PRIMARY KEY ,
+                         id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
                          value int unsigned
 );
 CREATE TABLE order(
-                      id int NOT NULL PRIMARY KEY ,
-                      order_number int AUTO_INCREMENT,
+                      id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                      order_number int ,
                       date_time timestamp,
-                      manager_id varchar,
+                      manager_id int,
                       client_id int,
                       client_addresses_id int,
                       discount_id int,
@@ -48,7 +48,7 @@ CREATE TABLE order(
                       FOREIGN KEY (discount_id) REFERENCES discount(id),
 );
 CREATE TABLE product_in_order(
-                                 id int NOT NULL PRIMARY KEY ,
+                                 id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
                                  product_id int,
                                  order_id int,
                                  count int unsigned,
