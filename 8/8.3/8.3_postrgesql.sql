a4164639cefd142ae29a91db588cca140fe0626a
Daria Postolatii <dariya2303@@gmail.com>
2022-08-30 19:05:24 +0300

    Добавлено задание 8.7

--- 8/8.3/8.3_postrgesql.sql
+++ 8/8.3/8.3_postrgesql.sql
@@ -4,12 +4,13 @@ CREATE TABLE users (
                        lastname char (64)           NOT NULL ,
                        firstname char (32)          NOT NULL ,
                        birthdate bytea              NOT NULL ,
-                       email char(255)              NOT NULL UNIQUE ,
-                       phone char(13)               NOT NULL UNIQUE  CHECK ( phone = 13),
+                       email char(255)              NOT NULL ,
+                       phone char(13)               NOT NULL UNIQUE,
                        gender gender_request        NOT NULL ,
                        timezone char(37)            NOT NULL ,
                        registration_time timestamp  NOT NULL ,
                        registration_ip char(15)     NOT NULL
 CHECK ( phone = '+^[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]?'),
-                       CHECK ( LOWER(email) )
+                       CHECK ( LOWER(email) ),
+                   UNIQUE (email)
 )
