2bd5a46886c24196a36a6387d1f3ee9732805252
Daria Postolatii <dariya2303@@gmail.com>
2022-09-18 11:55:04 +0300

    Додано завдання 11.3

---
+++ 11/11.3/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<?php
+    function capitalize ($cityName):string
+    {
+        $words = explode('-', $cityName);
+        for ($i = 0; $i < strlen($words); $i++)
+        {
+            if ($words[$i].indexOf('\'')!== -1 )
+            {
+                $wordsToArray = explode('\'',$words[$i]); // ['lend', 'liz']
+                $wordsToArray[0] = lcfirst($wordsToArray[0]);
+                $wordsToArray[1] = capitalize($wordsToArray[1]);
+                $words[$i]=$wordsToArray.join('\'');
+            }
+            else if ( $i === 0 || $i === strlen($words) -1 )
+            {
+                const cityName1 = strtoupper(substr("$words[$i]",0, 1));
+                const cityName2 = strtolower(substr("$words [$i]",1);
+                $words[$i] = cityName1 + cityName2;
+            }
+            else if ( strlen($words[$i]) >= 1 && strlen($words[$i] <= 3)
+            {
+                $words[$i] = strtolower($words[$i]);
+            }
+            else
+            {
+                $words[$i] = capitalize($words [$i]);
+            }
+        }
+        return $words.join('-');
+    }
+
+    let cityNameCapitalized = capitalize('Сен-сатюрнен-лез\'спт');
+    console.log( cityNameCapitalized );
+?>
+</body>
+</html>
